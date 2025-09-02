b2e8cdcaf08bfd82814d25707a4b789d7ad2f9a7
azu-wakinoza <wakinoza@gmail.com>
2025-09-02 09:44:54 +0900

    MainFrameクラスを作成

---
+++ src/main/java/todoApp/MainFrame.java
@@ -0,0 +1,32 @@
+package todoApp;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+/**.
+ * GUI表示を司るクラス
+ * JFrameの設定、必要なレイアウトマネージャやパネルの設置、コントローラークラスにリスナーを設定する
+ */
+public class MainFrame extends JFrame {
+
+  /**.
+   * コンストラクタ
+   *
+   * @param view    Viewクラスのインスタンスへの参照
+   * @param control Controlクラスのインスタンスへの参照
+   */
+  public MainFrame(View view, Control control) {
+    setTitle("シンプルなtodoアプリ");
+    setSize(1200, 800);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    control.makeTodoListPanel();
+
+    setLayout(new BorderLayout());
+
+    add(view.getTodoListScrollPane(), BorderLayout.NORTH);
+    add(view.getMemoPanel(), BorderLayout.CENTER);
+
+    control.setupMemoListeners();
+  }
+}
